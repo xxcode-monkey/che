ad68670c8c7ad4d45e985124d5ca6263b2ec1115
Aleksandr Andrienko <aandrienko@codenvy.com>
2016-11-30 22:38:45 +0200

    Fix Ctrl+S key combination.

--- ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/core/StandardComponentInitializer.java
+++ ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/core/StandardComponentInitializer.java
@@ -49,8 +49,6 @@ import org.eclipse.che.ide.actions.UndoAction;
 import org.eclipse.che.ide.actions.UploadFileAction;
 import org.eclipse.che.ide.actions.UploadFolderAction;
 import org.eclipse.che.ide.actions.find.FindActionAction;
-import org.eclipse.che.ide.api.action.Action;
-import org.eclipse.che.ide.api.action.ActionEvent;
 import org.eclipse.che.ide.api.action.ActionManager;
 import org.eclipse.che.ide.api.action.DefaultActionGroup;
 import org.eclipse.che.ide.api.action.IdeActions;
@@ -651,8 +649,6 @@ public class StandardComponentInitializer {
         editorTabContextMenu.add(splitHorizontallyAction);
         actionManager.registerAction(SPLIT_VERTICALLY, splitVerticallyAction);
         editorTabContextMenu.add(splitVerticallyAction);
-
-        actionManager.registerAction("noOpAction", new NoOpAction());
         actionManager.registerAction("signatureHelp", signatureHelpAction);
 
         DefaultActionGroup editorContextMenuGroup = new DefaultActionGroup(actionManager);
@@ -679,7 +675,6 @@ public class StandardComponentInitializer {
         keyBinding.getGlobal().addKey(new KeyBuilder().alt().charCode(KeyCodeMap.ARROW_LEFT).build(), "switchLeftTab");
         keyBinding.getGlobal().addKey(new KeyBuilder().alt().charCode(KeyCodeMap.ARROW_RIGHT).build(), "switchRightTab");
         keyBinding.getGlobal().addKey(new KeyBuilder().action().charCode('e').build(), "openRecentFiles");
-        keyBinding.getGlobal().addKey(new KeyBuilder().action().charCode('s').build(), "noOpAction");
         keyBinding.getGlobal().addKey(new KeyBuilder().charCode(KeyCodeMap.DELETE).build(), "deleteItem");
 
         keyBinding.getGlobal().addKey(new KeyBuilder().alt().charCode('N').build(), "newFile");
@@ -692,13 +687,6 @@ public class StandardComponentInitializer {
         } else {
             keyBinding.getGlobal().addKey(new KeyBuilder().alt().charCode('w').build(), "closeActiveEditor");
             keyBinding.getGlobal().addKey(new KeyBuilder().action().charCode('p').build(), "signatureHelp");
-        }
-    }
-
-    /** Action that does nothing. It's just for disabling (catching) browser's hot key. */
-    private class NoOpAction extends Action {
-        @Override
-        public void actionPerformed(ActionEvent e) {
         }
     }
 
